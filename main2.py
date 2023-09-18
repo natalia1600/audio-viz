6941d27e84b3d102c997208978432e2898c55612
jfaro <jfaro@umich.edu>
2023-09-18 01:00:18 -0400

    Plot data

--- main2.py
+++ main2.py
@@ -4,10 +4,12 @@ import wave
 import pyaudio
 import numpy as np
 from log import setup_logger
+import matplotlib.pyplot as plt
 
 logger = setup_logger("audioviz")
 
 FPS = 60
+DURATION = 30
 
 
 def main(argv):
@@ -34,6 +36,7 @@ def run_from_file(filepath):
 
     # Sample width in bytes
     sample_width = wavefile.getsampwidth()
+    dtype = get_dtype(sample_width)
 
     format = pa.get_format_from_width(sample_width)
     channels = wavefile.getnchannels()
@@ -58,11 +61,13 @@ def run_from_file(filepath):
     data = wavefile.readframes(chunk)
 
     while data:
-        stream.write(data)
-        data = wavefile.readframes(chunk)
+        # stream.write(data)
+        # data = wavefile.readframes(chunk)
+        data = wavefile.readframes(DURATION * rate)
 
         # TODO: create video frame from data
-        handle_chunk(data)
+        handle_chunk(data, dtype, rate)
+        break
 
     # Cleanup
     wavefile.close()
@@ -70,9 +75,32 @@ def run_from_file(filepath):
     pa.terminate()
 
 
-def handle_chunk(data, dtype):
+def get_dtype(sample_width: int):
+    if sample_width == 1:
+        return np.int8
+    if sample_width == 2:
+        return np.int16
+    logger.error(f"Invalid sample width: {sample_width}")
+    exit(1)
+
+
+def handle_chunk(data, dtype, sample_rate):
     logger.info("Handling frame")
-    sig = np.frombuffer(data, type=dtype)
+    sig = np.frombuffer(data, dtype)
+
+    plt.figure(1)
+
+    plot_a = plt.subplot(211)
+    plot_a.plot(sig)
+    plot_a.set_xlabel("sample rate * time")
+    plot_a.set_ylabel("energy")
+
+    plot_b = plt.subplot(212)
+    plot_b.specgram(sig, NFFT=1024, Fs=sample_rate, noverlap=900)
+    plot_b.set_xlabel("Time")
+    plot_b.set_ylabel("Frequency")
+
+    plt.show()
 
 
 if __name__ == "__main__":
