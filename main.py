2136347d568bde42a366ae9b28e012523d31a72e
Natalia Ramirez <natalia16@berkeley.edu>
2023-10-03 21:45:24 -0700

    plot sig data for left / right

--- main.py
+++ main.py
@@ -73,6 +73,8 @@ def run_from_file(filepath):
         # TODO: create video frame from data
         logger.info(f"Handling frame {frame_counter}")
         handle_chunk(data, dtype, rate)
+        # plot_sig_data(data, dtype, rate)
+        display_plots(data, dtype, rate)
         frame_counter += 1
         break
 
@@ -107,33 +109,39 @@ def handle_chunk(data, dtype, sample_rate):
     freq_R, amp_R = get_freq_amp(sample_rate, right)
 
 
-def plot_sig_data(data, dtype, sample_rate):
+def plot_sig_data(label, sig, sample_rate):
     T = 1 / sample_rate
     logger.info("Handling frame")
-    sig = np.frombuffer(data, dtype)
-    left, right = sig[0::2], sig[1::2]
+    # left, right = sig[0::2], sig[1::2]
 
     fig, (plot_a, plot_b, plot_c) = plt.subplots(3)
-    plot_a.plot(left)
+    plot_a.plot(sig)
     plot_a.set_xlabel("sample rate * time")
     plot_a.set_ylabel("energy")
 
-    left_size = left.size
-    fft_sig_abs = abs(fft(left))
-    xf_left = np.linspace(0.0, 1.0 / (2.0 * T), left_size // 2)
-    yf_left = 2 / left_size * np.abs(fft_sig_abs[0 : left_size // 2])
-    plot_b.semilogy(xf_left, yf_left)
+    sig_frequencies, sig_amplitudes = get_freq_amp(sample_rate, sig)
+    plot_b.semilogy(sig_frequencies, sig_amplitudes)
 
-    plot_b.set_xlabel("Left - Frequency")
-    plot_b.set_ylabel("Left - Amplitude")
+    plot_b.set_xlabel(label + " - Frequency")
+    plot_b.set_ylabel(label + " - Amplitude")
 
-    plot_c.specgram(left, NFFT=1024, Fs=T, noverlap=900)
+    sig_frequencies, sig_amplitudes = get_freq_amp(sample_rate, sig)
+    plot_b.semilogy(sig_frequencies, sig_amplitudes)
+
+    plot_c.specgram(sig, NFFT=1024, Fs=T, noverlap=900)
 
     plot_c.set_xlabel("Time")
     plot_c.set_ylabel("Frequency")
 
+    plt.suptitle(label + " Waveform")
+    
     plt.show()
 
+def display_plots(data, dtype, sample_rate):
+    sig = np.frombuffer(data, dtype)
+    left, right = sig[0::2], sig[1::2]
+    plot_sig_data("Left", left, sample_rate)
+    plot_sig_data("Right", right, sample_rate)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
