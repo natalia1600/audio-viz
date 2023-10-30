fixed scaling, visual changes to gui (colors, bar properties), needs to be cleaned up

gui.py
@@ -1,13 +1,17 @@
 import pygame
 import random
 import numpy as np
-
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from matplotlib.colors import LinearSegmentedColormap
 from log import setup_logger
 
 logger = setup_logger("gui ")
 
 COLOR_BG = (28, 28, 28)
-NUM_BARS = 100
+NUM_BARS = 120
+COLORS = ["#FFCC99", "#FFDAB9", "#77DD77",
+          "#FDFD96", "#99CCFF", "#77B5FE", "#FFEB3B"]
 
 
 class Visualizer:
@@ -17,10 +21,16 @@ class Visualizer:
 
         pygame.init()
         self.screen = pygame.display.set_mode((screen_width, screen_height))
-        
+
+        # save previous wave heights for smoothing
+        self.prev_ys = np.full(
+            shape=NUM_BARS,
+            fill_value=screen_height // 4,
+            dtype=np.int64
+        )
+
         self.max_amplitude = 0
-        
-        
+
     def should_quit(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -29,30 +39,39 @@ class Visualizer:
                 return True
         return False
 
-
     def quit(self):
         pygame.quit()
 
-
-    def draw_bar(self, x, y, width, height, color):
-        pygame.draw.rect(self.screen, color, pygame.Rect(x, y - height, width, height))
-
+    def draw_bar(self, x, y, width, height):
+        # colormap = plt.cm.get_cmap('cubehelix')
+        colormap = plt.cm.tab20b
+        print('colormap', colormap)
+        color = colormap(height / (self.screen_height / 2))
+        rgb = mcolors.to_rgb(color)
+        pygame_color = tuple(int(round(c * 255)) for c in rgb)
+        # convert color to RGB
+        print(pygame_color)
+        pygame.draw.rect(self.screen, pygame_color, pygame.Rect(
+            x, y - height, width, height), border_radius=width//2)
 
     def draw_bars(self, xs, ys):
         print("xs ", xs[:20])
         print("ys", ys[:20])
-
+        print(xs)
+        print(ys)
         num_points = xs.size
+        xs = np.arange(NUM_BARS)
+        # num xs points per NUM_BARS
         step = num_points // NUM_BARS
-        bar_width = self.screen_width // NUM_BARS
+
+        # size of each bar according to screen width
+        bar_width = (self.screen_width // NUM_BARS) * 2
+
         logger.info(f"Number of points provided to draw bars {num_points}")
-
-
 
         # start = math.log(1, 10)
         # stop = math.log(24000, 10)
         # ys_to_sample = np.logspace(start, stop, num=NUM_BARS)
-
 
         # print("ys_to sample", ys_to_sample)
 
@@ -67,24 +86,76 @@ class Visualizer:
         # exit(1)
 
         # VALID NO TOUCH
-        ys_samples = ys[::step]
-        xs = np.arange(0, self.screen_width, self.screen_width // NUM_BARS)
 
+        # Set ys to ys point at step intervals
+        # xs = np.logspace(0, np.log10(self.screen_width), NUM_BARS)
+
+        EXTRA_SPACE = NUM_BARS // 20
+        y_logspace = np.logspace(0, np.log10(
+            len(ys)), num=NUM_BARS + EXTRA_SPACE)
+        y_logspace = y_logspace[EXTRA_SPACE //
+                                2: len(y_logspace) - (EXTRA_SPACE // 2)][:NUM_BARS]
+        indices = np.floor(y_logspace).astype(int)
+        y_exp = np.arange(0, NUM_BARS) ** 1.5
+
+        # Make sure indices are within the range of ys_samples
+        indices = np.clip(indices, 0, len(ys) - 1)
+        ys_samples = ys[indices]
+
+        print(ys_samples, 'ys_samples1')
+
+        ys_samples = ys_samples * y_exp
+
+        print(ys_samples, 'ys_samples * y_exp')
+        print(ys_samples, 'ys_samples_logged')
+        print(indices, 'indices')
+        print("ys_samples_logged", ys_samples)
+        print("ys_samples_logged size:", len(ys_samples))
+
+        # set xs to NUM_BARS evenly spaced points accross the screen
+        xs = np.arange(0, self.screen_width, self.screen_width // NUM_BARS)
+        # set max_amplitude to the larger value between curr max_amplitude
+        # and the max amplitude of ys
         self.max_amplitude = max(self.max_amplitude, np.max(ys_samples))
-        scale_factor = self.screen_height / self.max_amplitude
+
+        # calculates scale factor to ensure the y-values will fit
+        # within the screen height
+        # add 1 to max amplitude to avoid zero division
+        # Leave room for edges of screen by scaling (max_amplitude + 1) by 1.5
+        scale_factor = (self.screen_height * 0.5) / (self.max_amplitude + 1)
+
+        # scales the values in the ys_samples array by a factor of scale_factor
         ys_samples_scaled = ys_samples * scale_factor
 
-        for x, y in zip(xs, ys_samples_scaled):
-            color = (255, 255, 255)
-            self.draw_bar(x, self.screen_height // 2, bar_width, y, color)
+        # maybe num is num_bars?
+        values = np.logspace(0, np.log10(scale_factor), num=num_points)
 
+        # print("xs", xs)
+        print("xs size", len(xs))
 
+        # print("ys_samples_scaled", ys_samples_scaled)
+        print("ys_samples_scaled size", len(ys_samples_scaled))
+
+        # print("values", values)
+        print("values size", len(values))
+
+        ys_smoothed = np.average(
+            np.vstack((ys_samples_scaled, self.prev_ys)), axis=0, weights=[0.5, 0.5])
+
+        self.prev_ys = ys_smoothed
+
+        xs = xs[4::4]
+        ys_smoothed = ys_smoothed[4::4]
+
+        for x, y in zip(xs, ys_smoothed):
+            self.draw_bar(x + (bar_width / 2), (self.screen_height // 2) + y,
+                          bar_width, y * 2)
 
     def update(self, xs, ys):
         logger.info("Creating new frame")
 
         # Set background
-        self.screen.fill(COLOR_BG)
+        self.screen.fill('BLACK')
 
         # Window contents
         self.draw_bars(xs, ys)

main.py
@@ -104,7 +104,6 @@ def run_from_file(filepath):
     pa.terminate()
 
 
-
 def get_dtype(sample_width: int):
     if sample_width == 1:
         return np.int8
@@ -119,7 +118,7 @@ def get_freq_amp(sample_rate, samples):
     num_samples = samples.size
     fft_sig_abs = abs(fft(samples))
     frequencies = np.linspace(0.0, 1.0 / (2.0 * T), num_samples // 2)
-    amplitudes = 2 / num_samples * np.abs(fft_sig_abs[0 : num_samples // 2])
+    amplitudes = 2 / num_samples * np.abs(fft_sig_abs[0: num_samples // 2])
     return (frequencies, amplitudes)
 
 